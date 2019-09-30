359f36858410799d7f8eec84a8e271e64a76e63c
Evan Vogts <evanjvogts@gmail.com>
2019-09-30 11:02:34 -0500

    step 3

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/adam';
 
 const getItems = function()  {
-  fetch('https://thinkful-list-api.herokuapp.com/adam/items')
+  fetch(`${BASE_URL}/items`)
     .then (res => res.json())
     .then (resonseJson => console.log(resonseJson));
   return Promise.resolve('A successful response!');
